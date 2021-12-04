126b9ff8b5672cc04eb70651f45f6c4ce7fe0d55
sperlik-oliver <75211235+sperlik-oliver@users.noreply.github.com>
2021-12-04 14:16:40 +0100

    s

--- Resolvers/Branches/BranchQueries.cs
+++ Resolvers/Branches/BranchQueries.cs
@@ -1,13 +1,18 @@
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 using dotnet_graphql_hotchocolate_abdot_middleware_api.Services.Classes;
 using dotnet_graphql_hotchocolate_abdot_middleware_api.Services.Interfaces;
 using dotnet_graphql_hotchocolate_abdot_middleware_api.Models;
+using dotnet_graphql_hotchocolate_abdot_middleware_api.Utilities;
 using HotChocolate.Types;
 
 namespace dotnet_graphql_hotchocolate_abdot_middleware_api.Resolvers.Branches {
+    
     [ExtendObjectType(Name = "Query")]
     public class BranchQueries {
+        private readonly string uri = "https://abdot-api.herokuapp.com/api/branch";
         private IBranchService branchService;
 
         public BranchQueries() {
@@ -15,7 +20,17 @@ namespace dotnet_graphql_hotchocolate_abdot_middleware_api.Resolvers.Branches {
         }
 
         public async Task<List<Models.Branch>> GetBranches() {
-            return await branchService.GetBranchesAsync();
+            using HttpClient httpClient = new HttpClient();
+            HttpResponseMessage responseMessage = await httpClient.GetAsync($"{uri}/es");
+            if (!responseMessage.IsSuccessStatusCode) {
+                throw new Exception($"Error: {responseMessage.StatusCode}, {responseMessage.ReasonPhrase}");
+            }
+
+            var result = await responseMessage.Content.ReadAsStringAsync();
+
+            List<Branch> branches = (List<Branch>) CustomJsonSerialization.Deserialize(result);
+
+            return branches;
         }
 
         public async Task<Models.Branch> GetBranch(int id) {
