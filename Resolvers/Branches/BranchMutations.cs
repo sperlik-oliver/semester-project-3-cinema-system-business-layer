2fc6a54a57c248b9a850087b6703d80b16930820
Tymon2115 <tymon.podlowski@gmail.com>
2021-12-05 17:38:26 +0100

    BranchMutations finished

---
+++ Resolvers/Branches/BranchMutations.cs
@@ -0,0 +1,43 @@
+using System.Threading.Tasks;
+using dotnet_graphql_hotchocolate_abdot_middleware_api.Models;
+using dotnet_graphql_hotchocolate_abdot_middleware_api.Services.Classes;
+using dotnet_graphql_hotchocolate_abdot_middleware_api.Services.Interfaces;
+using HotChocolate.Types;
+
+namespace dotnet_graphql_hotchocolate_abdot_middleware_api.Resolvers.Branches {
+    [ExtendObjectType(Name = "Mutation")]
+    public class BranchMutations {
+        private IBranchService branchService;
+        
+
+        public BranchMutations() {
+            branchService = new BranchService();
+        }
+
+        public async Task<Branch> CreateBranch(BranchInput input) {
+            //TODO oliver do we create branch like that with halls already in or we make method for adding halls to it>
+            //TODO also with employees
+            var branch = new Branch {
+                City = input.City,
+                Street = input.Street,
+                Postcode = input.PostCode,
+                Country = input.Street,
+                Halls = input.Halls,
+                Employees = input.Employees,
+            };
+            return await branchService.CreateBranchAsync(branch);
+        }
+
+        public async Task<Branch> EditBranch(BranchInput input) {
+            var branch = new Branch {
+                City = input.City,
+                Street = input.Street,
+                Postcode = input.PostCode,
+                Country = input.Street,
+                Halls = input.Halls,
+                Employees = input.Employees,
+            };
+            return await branchService.EditBranchAsync(branch);
+        }
+    }
+}
